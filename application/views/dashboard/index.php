49425153d7cb7c48df35e732105fe9a41395f3a3
César Salomél <c.silvasalome@hotmail.com>
2022-01-14 16:59:45 -0300

    titulo

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -19,7 +19,7 @@
             <div class="card-header pb-0">
                 <div class="row">
                     <div class="col-lg-6 col-7">
-                        <h6>Gráfico Geral de ocupação - <span style='font-size: 12px' id='data_ult_att' name='data_ult_att'></span></h6>
+                        <h6>Gráfico Geral de Ocupação - <span style='font-size: 12px' id='data_ult_att' name='data_ult_att'></span></h6>
                     </div>
                     <div class="col-lg-6 col-5 my-auto text-end">
                         <div class="dropdown float-lg-end pe-4">

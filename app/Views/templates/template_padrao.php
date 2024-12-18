02bb5f46d8185ea3e1c5d6e3db31518953fe1696
Cesar Silva Salome <c.silvasalome@hotmail.com>
2024-12-18 09:17:26 -0300

    Ajustando css!!

--- app/Views/templates/template_padrao.php
+++ app/Views/templates/template_padrao.php
@@ -28,21 +28,21 @@ if(isset($diretorio_raiz) && strlen($diretorio_raiz)>0){
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="public/assets/img/hmdcc2.ico">
-  <link rel="icon" type="image/png" href="<? echo base_url("public/assets/img/hmdcc2.ico");?>">
+  <link rel="icon" type="image/png" href="<?php echo base_url("public/assets/img/hmdcc2.ico");?>">
   <title>
     HMDCC
 </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
-  <link href="<? echo base_url("public/assets/css/nucleo-icons.css");?>" rel="stylesheet" />
-  <link href="<? echo base_url("public/assets/css/nucleo-svg.css");?>" rel="stylesheet" />
+  <link href="<?php echo base_url("public/assets/css/nucleo-icons.css");?>" rel="stylesheet" />
+  <link href="<?php echo base_url("public/assets/css/nucleo-svg.css");?>" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script> -->
-  <link href="<? echo base_url("public/assets/css/nucleo-svg.css");?>" rel="stylesheet" />
+  <link href="<?php echo base_url("public/assets/css/nucleo-svg.css");?>" rel="stylesheet" />
   <!-- CSS Files -->
   <link id="pagestyle" href="<? echo base_url("public/assets/css/soft-ui-dashboard.css?v=1.0.3");?>" rel="stylesheet" />
   <style>
